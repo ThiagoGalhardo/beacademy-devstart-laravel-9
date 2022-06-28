9ef5c61bcccec61d3c4442eb5f4c79bf30811404
Thiago Galhardo <thiago.galhardo@hotmail.com>
2022-06-28 17:06:28 -0300

    migrations

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
+use App\Http\Controllers\{
+    UserController,
+    ViaCepController
+};
 
 /*
 |--------------------------------------------------------------------------
@@ -20,6 +23,10 @@ Route::get('/', function () {
 
 Route::get('/users', [UserController::class, 'index'])->name('users.index');
 Route::get('/users/{id}', [UserController::class, 'show'])->name('users.show');
+
+Route::get('viacep', [ViaCepController::class, 'index'])->name('viacep.index');
+Route::post('viacep', [ViaCepController::class, 'index'])->name('viacep.index.post');
+Route::get('viacep/{cep}', [ViaCepController::class, 'show'])->name('viacep.show');
 
 // Route::get('hello-world', function(){
 //     echo 'Hello World';
