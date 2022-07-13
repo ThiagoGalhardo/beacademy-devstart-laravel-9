9f352079c5f788ce8c28cfb215544c93f4239ede
Thiago Galhardo <thiago.galhardo@hotmail.com>
2022-07-12 21:45:03 -0300

    install breeze

--- resources/views/template/users.blade.php
+++ resources/views/template/users.blade.php
@@ -26,9 +26,34 @@
                 </ul>
             </div>
 
+            <div class="col-2">
+                <ul class="navbar-nav mr-auto">
+                    @if(Auth::user())
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="#">{{ Auth::user()->name }}</a>
+                    </li>
+                    <li class="nav-item">
+                        <form method="POST" action="{{ route('logout') }}">
+                            @csrf
+                            <x-responsive-nav-link class="nav-link text-white" :href="route('logout')" onclick="event.preventDefault();
+                                                                this.closest('form').submit();">
+                                {{ __('Sair') }}
+                            </x-responsive-nav-link>
+                        </form>
+                    </li>
+                    @else
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="{{ route('login') }}">Entrar</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="{{ route('register') }}">Registrar-se</a>
+                    </li>
+                    @endif
+                </ul>
+            </div>
+
+
         </nav>
-
-
 
 
 
--- routes/web.php
+++ routes/web.php
@@ -7,20 +7,15 @@ use App\Http\Controllers\{
     ViaCepController
 };
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+require __DIR__ . '/auth.php';
 
 Route::get('/', function () {
     return redirect()->route('users.index');
 });
+
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth'])->name('dashboard');
 
 Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
 Route::get('/users/{id}/posts', [PostController::class, 'show'])->name('posts.show');
